53ece6badfced1f0c12b8fcf313657bacfac75ce
Skylake02 <mattngaw02@gmail.com>
2018-08-17 21:08:39 -0400

    finished Set datatype section

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 window.onload = function() {
 
-	let names = new Set();
-
-	names.add('Tom').add('Jak').add('Crystal');
-
-	names.clear();
-	console.log(names.size);
-	console.log(names);
-
+	
 
 }
